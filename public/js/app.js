ddc7cb64daf449ed9943f21609abf3a5b05e8320
Priya <priyagrover@illuminz.com>
2017-05-01 14:45:49 +0530

    Final custom stle

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,46 @@
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
-
 var socket = io();
 
-console.log(name + ' wants to join '+ room);
+console.log(name + ' wants to join ' + room);
 
-socket.on('connect',function(){
-	console.log('Connect to socket.io server');
+// Update h1 tag
+jQuery('.room-title').text(room);
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
-socket.on('message',function(message){
-	var $message = jQuery('.messages');
+socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>');
+
+
 	console.log('New message:');
 	console.log(message.text);
-	$message.append('<p><strong>'+ message.name +' '+ momentTimestamp.local().format('h:mm a') +'</strong></p>');
-	$message.append('<p>'+message.text+'</p>');
+
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
 });
 
-// Handles submiting of new message
+// Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit',function(event){
+$form.on('submit', function (event) {
 	event.preventDefault();
-	var $message = $form.find('input[name=message]');
-	socket.emit('message',{
-		name : name,
-		text:$message.val()
-	});
-	$message.val('');
 
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
+	});
+
+	$message.val('');
 });
